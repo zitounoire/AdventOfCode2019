053da37ed9156fa4260608daa4448c8a92cea9df
zee <zitounoire@protonmail.com>
2020-12-03 21:45:19 +0100

    refactored fonction calls for part2

--- 2020/day3/solution.py
+++ 2020/day3/solution.py
@@ -1,22 +1,20 @@
 #!/usr/bin/python3
+import numpy
 
 def bi_slide(right, skip):
-    x = 0
-    sum = 0
+    x = sum =0
     for line in trail:
-        if skip and trail.index(line) % 2 != 0:
+        if skip and trail.index(line) %2 != 0:
             continue
-        if line[x] == '#':
+        if (line[x] == '#'):
             sum +=1
         x += right
         if x > 30:
-            x -= 31
+            x -= 31 
     return sum
 
 trail = [line.strip('\n') for line in open('input.txt','r').readlines()]
 
-# day 1
-print('Part 1: ', bi_slide(3,False))
+print('Part 1: ', bi_slide(3,False)) # day 1
 
-# day 2
-print('Part 2: ', bi_slide(1,False) * bi_slide(3,False) * bi_slide(5,False) * bi_slide(7,False) * bi_slide(1, True))
+print('Part 2: ', numpy.prod([bi_slide(i,bool(i==2)) for i in [1,2,3,5,7]])) # day 2
