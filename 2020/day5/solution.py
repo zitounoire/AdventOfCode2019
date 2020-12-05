4e72c05978792b42d48447087981417c5dfed8c5
zee <zitounoire@protonmail.com>
2020-12-05 20:37:44 +0100

    day 5

---
+++ 2020/day5/solution.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+billets = [item.strip() for item in open('input.txt','r').readlines()]
+
+# row * 8 + col
+IDs_list = [(int(billet[:7].replace('F','0').replace('B','1'),2) * 8 + int(billet[7:].replace('L','0').replace('R','1'),2)) for billet in billets]
+
+test =[15,944,945,946,947]
+for row in range(2,118):
+    for col in range(8):
+        test.append(row*8+col)
+
+print('Part 1:', max(IDs_list))
+print('Part 2:',set(test).symmetric_difference(set(IDs_list)))
